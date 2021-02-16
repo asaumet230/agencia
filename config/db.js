73a41d1ebd6359f74e1f85e88acd25c4c2c773cc
asaumet230 <69801361+asaumet230@users.noreply.github.com>
2021-02-16 06:41:32 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'postgres',
+    protocol: 'postgres',
     define: {
         timestamps: false
     }, pool: {
